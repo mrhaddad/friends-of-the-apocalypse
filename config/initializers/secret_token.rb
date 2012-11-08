51a0327b24103f3b4c75714bc63dd9cd5bb93b89
Michael Haddad <mrhaddad@unknown7cd1c3dce3a9.gateway.2wire.net>
2012-11-08 12:12:45 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fota::Application.config.secret_token = 'ee640c64ce3438d756ab50369311a8e9d25324f6879d7dd71538f17ff1919eb3890a20f61e929daee72c33dd6d2b07de5e8ccc33cc93c653c5276f55d814dd03'
